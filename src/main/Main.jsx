f53bd7f8b1d09192620c3add9a89ca40717019cd
Marcel Szewczyk <marcel.szewczyk@gmail.com>
2021-09-13 21:30:08 +0200

    Fetching details from API added

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./Main.css";
 
 function Main() {
   const [coinList] = useState([
-    { key: "Bitcoin", value: "btc" },
-    { key: "Ethereum", value: "eth" },
-    { key: "Dogecoin", value: "doge" },
-    { key: "Tether", value: "usdt" },
-    { key: "Polkadot", value: "dot" },
+    { key: "Bitcoin", value: "90" },
+    { key: "Ethereum", value: "80" },
+    { key: "Dogecoin", value: "2" },
+    { key: "Tether", value: "518" },
+    { key: "Polkadot", value: "45219" },
   ]);
+  const [selectedCoin, setSelectedCoin] = useState(null);
+  const [currentCoinData, setCurrentCoinData] = useState(null);
+
+  useEffect(() => {
+    if (selectedCoin === null) {
+      return;
+    }
+
+    fetch(`https://api.coinlore.net/api/ticker/?id=${selectedCoin}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setCurrentCoinData(result[0]);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }, [selectedCoin]);
 
   return (
     <div className="main-component">
@@ -23,6 +42,7 @@ function Main() {
           id="select-main"
           data-testid="select-main"
           defaultValue=""
+          onChange={(event) => setSelectedCoin(event.target.value)}
         >
           <option value="" disabled>
             Select...
@@ -34,6 +54,11 @@ function Main() {
           ))}
         </select>
       </section>
+      {currentCoinData && <section className="coin-details">
+        Symbol: {currentCoinData.symbol}<br />
+        Market Cap: {currentCoinData.market_cap_usd} USD<br />
+        Price: {currentCoinData.price_usd} USD<br />
+      </section>}
     </div>
   );
 }
